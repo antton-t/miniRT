97373a344c95a1f87d39d8e55b06eea57e619f48
antton-t <64638111+antton-t@users.noreply.github.com>
2021-04-27 15:32:12 +0200

    Add files via upload

---
+++ includes/ft_miniRT.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_miniRT.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antton-t <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/15 15:46:51 by antton-t          #+#    #+#             */
+/*   Updated: 2021/02/10 09:38:47 by antton-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+void    ft_is_light(char **line);
+void    ft_parse(char **line);
+int	ft_check_argv(char *str, char *end);
+
+#endif
